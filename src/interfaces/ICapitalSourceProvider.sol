ff7dbf6f4ad37a28d42779bb42af122baaf0eb7a
tem.eth <artem.bv@pm.me>
2024-02-27 12:56:59 +0100

    feat: add capital source provider interface draft

---
+++ src/interfaces/ICapitalSourceProvider.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.23;
+
+/**
+ * @author  .
+ * @title   Capital Source Provider
+ * @dev     .
+ * @notice  Capital Source Provider is used as a mechanism to deliver capital from external protocols to Octant
+ */
+
+interface ICapitalSourceProvider {
+    function isWithdrawalRequestRequired() external returns (bool);
+    function availableToWithdraw() external returns (bool);
+    function requestPgWithdrawal() external returns (uint256 estimatedTimeToWithdraw);
+    function withdrawAccumulatedPgCapital() external returns (uint256 pgAmount);
+}
