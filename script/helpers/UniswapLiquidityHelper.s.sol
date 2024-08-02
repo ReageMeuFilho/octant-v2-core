5a68a46571a7029a6a630ffa92a62fbf32a014e1
Tem <artem.bv@pm.me>
2024-08-02 21:55:06 +0100

    feat: fix tick range rounding error & deploy uniswap liquidity helper

--- script/helpers/UniswapLiquidityHelper.s.sol
+++ script/helpers/UniswapLiquidityHelper.s.sol
@@ -2,11 +2,34 @@
 pragma solidity ^0.8.23;
 pragma abicoder v2;
 
+import {ABDKMath64x64} from 'abdk-libraries-solidity/ABDKMath64x64.sol';
 import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 // import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
+
+library ExtraTickMath {
+    function divRound(int128 x, int128 y) internal pure returns (int128 result) {
+        int128 quot = ABDKMath64x64.div(x, y);
+        result = quot >> 64;
+
+        // Check if remainder is greater than 0.5
+        if (quot % 2**64 >= 0x8000000000000000) {
+            result += 1;
+        }
+    }
+
+    function nearestUsableTick(int24 tick_, uint24 tickSpacing) internal pure returns (int24 result) {
+        result = int24(divRound(int128(tick_), int128(int24(tickSpacing)))) * int24(tickSpacing);
+
+        if (result < TickMath.MIN_TICK) {
+            result += int24(tickSpacing);
+        } else if (result > TickMath.MAX_TICK) {
+            result -= int24(tickSpacing);
+        }
+    }
+}
 
 contract UniswapLiquidityHelper is IERC721Receiver {
 
@@ -28,7 +51,7 @@ contract UniswapLiquidityHelper is IERC721Receiver {
     }
 
     mapping(uint256 => Deposit) public deposits;
-    mapping(uint24 => int24) public override feeAmountTickSpacing;
+    mapping(uint24 => int24) public feeAmountTickSpacing;
 
     constructor(address token0Address_, address token1Address_, address nonfungiblePositionManager_, uint24 poolFee_) {
         contractOwner = msg.sender;
@@ -42,8 +65,7 @@ contract UniswapLiquidityHelper is IERC721Receiver {
         feeAmountTickSpacing[10000] = 200;
     }
 
-    function onERC721Received(
-                              address operator,
+    function onERC721Received(address operator,
                               address,
                               uint256 tokenId,
                               bytes calldata
@@ -53,20 +75,12 @@ contract UniswapLiquidityHelper is IERC721Receiver {
         return this.onERC721Received.selector;
     }
 
-    function _createDeposit(address owner, uint256 tokenId) internal {
-        (, , address token0, address token1, , , , uint128 liquidity, , , , ) =
-            nonfungiblePositionManager.positions(tokenId);
-
-        // set the owner and data for position
-        // operator is msg.sender
-        deposits[tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
-    }
     /// @notice Calls the mint function defined in periphery, mints the same amount of each token
     /// @return tokenId The id of the newly minted ERC721
     /// @return liquidity The amount of liquidity for the position
     /// @return amount0 The amount of token0
     /// @return amount1 The amount of token1
-    function mintNewPosition(uint256 amountToMint0ETH_, uint256 amountToMint1ETH_)
+    function mintNewPosition(uint256 amountToMint0Wei_, uint256 amountToMint1Wei_)
         external
         returns (
             uint256 tokenId,
@@ -74,24 +88,24 @@ contract UniswapLiquidityHelper is IERC721Receiver {
             uint256 amount0,
             uint256 amount1
         ) {
-        uint256 amount0ToMint = 1 ether * amountToMint0ETH_;
-        uint256 amount1ToMint = 1 ether * amountToMint1ETH_;
+        uint256 amount0ToMint = amountToMint0Wei_;
+        uint256 amount1ToMint = amountToMint1Wei_;
 
         // Approve the position manager
         TransferHelper.safeApprove(token0Address, address(nonfungiblePositionManager), amount0ToMint);
         TransferHelper.safeApprove(token1Address, address(nonfungiblePositionManager), amount1ToMint);
 
         int24 tickSpacing = feeAmountTickSpacing[poolFee];
-        int24 tickLowerRounded = nearestUsableTick(TickMath.MIN_TICK, tickSpacing);
-        int24 tickUpperRounded = nearestUsableTick(TickMath.MAX_TICK, tickSpacing);
+        int24 tickLowerRounded = ExtraTickMath.nearestUsableTick(TickMath.MIN_TICK, uint24(tickSpacing));
+        int24 tickUpperRounded = ExtraTickMath.nearestUsableTick(TickMath.MAX_TICK, uint24(tickSpacing));
 
         INonfungiblePositionManager.MintParams memory params =
             INonfungiblePositionManager.MintParams({
                 token0: token0Address,
                 token1: token1Address,
                 fee: poolFee,
-                tickLower: TickMath.MIN_TICK,
-                tickUpper: TickMath.MAX_TICK,
+                tickLower: tickLowerRounded,
+                tickUpper: tickUpperRounded,
                 amount0Desired: amount0ToMint,
                 amount1Desired: amount1ToMint,
                 amount0Min: 0,
@@ -120,16 +134,6 @@ contract UniswapLiquidityHelper is IERC721Receiver {
         }
     }
 
-    function nearestUsableTick(int24 tick_, uint24 tickSpacing) internal pure returns (int24 result) {
-        result = int24(divRound(int128(tick_), int128(int24(tickSpacing)))) * int24(tickSpacing);
-
-        if (result < TickMath.MIN_TICK) {
-            result += int24(tickSpacing);
-        } else if (result > TickMath.MAX_TICK) {
-            result -= int24(tickSpacing);
-        }
-    }
-
 
     function returnFunds(uint256 token0Amount, uint256 token1Amount) public {
         if (msg.sender != contractOwner) {
@@ -137,5 +141,14 @@ contract UniswapLiquidityHelper is IERC721Receiver {
         } 
         TransferHelper.safeTransfer(token0Address, contractOwner, token0Amount);
         TransferHelper.safeTransfer(token1Address, contractOwner, token1Amount);
+    }
+
+    function _createDeposit(address owner, uint256 tokenId) internal {
+        (, , address token0, address token1, , , , uint128 liquidity, , , , ) =
+            nonfungiblePositionManager.positions(tokenId);
+
+        // set the owner and data for position
+        // operator is msg.sender
+        deposits[tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
     }
 }
