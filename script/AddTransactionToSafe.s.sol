d3442be442be6127bf95822126b6db62fd4f0516
Tabish Shaikh <bonjour.tabish@gmail.com>
2024-09-25 16:32:51 +0530

    add: approve + mint script

--- script/AddTransactionToSafe.s.sol
+++ script/AddTransactionToSafe.s.sol
@@ -11,15 +11,19 @@ contract AddTransactionToSafe is BatchScript {
 
     function setUp() public {
         safe_ = vm.envAddress("SAFE_ADDRESS");
-        dragonVaultModule = vm.envAddress("SAFE_ADDRESS");
+        dragonVaultModule = vm.envAddress("SAFE_DRAGON_VAULT_MODULE_ADDRESS");
     }
 
     function run() public isBatch(safe_) {
+        bytes memory txn1 = abi.encodeWithSignature("approve(address,uint256)", dragonVaultModule, 100e18);
+
+        addToBatch(safe_, 0, txn1);
+
         bytes memory data = abi.encodeWithSignature("mint(uint256,address)", 1e18, safe_);
 
-        bytes memory txn = abi.encodeWithSignature("exec(bytes)", data);
+        bytes memory txn2 = abi.encodeWithSignature("exec(bytes)", data);
 
-        addToBatch(dragonVaultModule, 0, txn);
+        addToBatch(dragonVaultModule, 0, txn2);
 
         executeBatch(true);
     }
