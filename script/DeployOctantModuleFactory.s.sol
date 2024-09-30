461de870cfc675a067b7cac40860439c51808ac2
Tabish Shaikh <bonjour.tabish@gmail.com>
2024-09-30 13:47:29 +0530

    feat: add docs

---
+++ script/DeployOctantModuleFactory.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ModuleProxyFactory } from "../src/dragons/ModuleProxyFactory.sol";
+import { OctantRewardsSafe } from "../src/dragons/modules/OctantRewardsSafe.sol";
+import "forge-std/Script.sol";
+
+contract DeployOctantModuleFactory is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        ModuleProxyFactory factory = new ModuleProxyFactory();
+
+        OctantRewardsSafe octantModule = new OctantRewardsSafe();
+
+        vm.stopBroadcast();
+
+        // Log the address of the newly deployed Safe
+        console.log("Factory deployed at:", address(factory));
+        console.log("Octant Safe Module deployed at:", address(octantModule));
+    }
+}
