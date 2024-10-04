3ae22e74d09b3aa2328060f9cfd36a36f8fca495
Tabish Shaikh <bonjour.tabish@gmail.com>
2024-10-04 15:32:00 +0530

    feat: initial base strategy

--- test/mocks/MockFailSafe.sol
+++ test/mocks/MockFailSafe.sol
@@ -6,11 +6,11 @@ import { Enum } from "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
 /// @dev Mock Safe that returns false on execTransactionFromModule
 contract FailSafe {
     function execTransactionFromModule(
-        address to,
-        uint256 value,
-        bytes memory data,
-        Enum.Operation operation
-    ) external returns (bool success) {
+        address /*to*/,
+        uint256 /*value*/,
+        bytes memory /*data*/,
+        Enum.Operation /*operation*/
+    ) external pure returns (bool success) {
         success = false;
     }
 }
