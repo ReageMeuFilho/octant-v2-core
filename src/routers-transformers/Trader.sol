e902b55eee1ff0730e69d13c29972db29a28a9f1
Pawel Peregud <paulperegud@protonmail.ch>
2024-10-11 14:34:40 +0200

    add Trader - it enables swap from ETH to GLM at random times

---
+++ src/routers-transformers/Trader.sol
@@ -0,0 +1,131 @@
+/* SPDX-License-Identifier: UNLICENSED */
+pragma solidity ^0.8.23;
+
+import "forge-std/console.sol";
+
+import {Ownable} from "@solady/auth/Ownable.sol";
+import "solady/src/utils/SafeCastLib.sol";
+
+/// @author .
+/// @title Octant Trader
+/// @notice Octant Trader is a contract that performs "DCA" in terms of sold token into another token.
+/// @dev This contract performs trades in a random times, attempting to isolate the deployer from risks of insider trading.
+contract Trader is Ownable {
+    event Conversion(uint256 sold, uint256 bought);
+    event Status(uint256 ethBalance, uint256 glmBalance);
+
+    uint256 public constant blocksADay = 7200;
+
+    /// @notice Chance is probability of a trade occuring at a particular height, normalized to uint256.max instead to 1
+    uint256 public chance;
+
+    /// @notice How much ETH can be spend per day on average (upper bound)
+    uint256 public spendADay;
+
+    /// @notice Spent ETH since startingBlock
+    uint256 public spent = 0;
+
+    /// @notice Rules for spending were last updated at this height.
+    uint256 public startingBlock = block.number;
+
+    /// @notice Lowest allowed size of a sale.
+    uint256 public saleValueLow;
+
+    /// @notice Highest allowed size of a sale.
+    /// @dev Technically, this value minus one wei.
+    uint256 public saleValueHigh;
+
+    /// @notice Height of block hash used as a last source of randomness.
+    uint256 public lastHeight;
+
+    uint256 public lastSold = 0;
+
+    /// @notice Heights at which `convert()` can be called is decided by randomness.
+    ///         If you see `Trader__WrongHeight()` error, retry in the next block.
+    ///         Note, you don't need to pay for gas to learn if tx will apply!
+    error Trader__WrongHeight();
+
+    /// @notice This error indicates that contract has spent more ETH than allowed.
+    ///         Retry tx in the next block.
+    error Trader__SpendingTooMuch();
+
+    /// @notice Raised if tx was already performed for this source of randomness (this height).
+    error Trader__RandomnessAlreadyUsed();
+
+    /// @notice Unsafe randomness seed.
+    error Trader__RandomnessUnsafeSeed();
+
+    /// @notice This one indicates software error. Should never happen.
+    error Trader__SoftwareError();
+
+    constructor() payable {
+        _initializeOwner(msg.sender);
+    }
+
+    /// @notice Transfers funds that are to be converted by to target token by external converter.
+    /// @param height that will be used as a source of randomness. One height value can be used only once.
+    function convert(uint256 height) public {
+        uint256 rand = getRandomNumber(height);
+        if (rand > chance) revert Trader__WrongHeight();
+        if (hasOverspent(height)) revert Trader__SpendingTooMuch();
+        if (lastHeight >= height) revert Trader__RandomnessAlreadyUsed();
+        lastHeight = height;
+
+        uint256 saleValue = getUniformInRange(saleValueLow, saleValueHigh, rand);
+        if (saleValue > saleValueHigh) revert Trader__SoftwareError();
+
+        // this simulates sending ETH to swapper
+        (bool success,) = payable(owner()).call{value: saleValue}("");
+        require(success);
+
+        lastSold = saleValue;
+        spent = spent + saleValue;
+    }
+
+    function canTrade(uint256 height) public view returns (bool) {
+        uint256 rand = getRandomNumber(height);
+        return (rand <= chance);
+    }
+
+    function hasOverspent(uint256 height) public view returns (bool) {
+        return (spent > (height - startingBlock) * (spendADay / blocksADay));
+    }
+
+    /// @notice Sets ETH spending limits.
+    /// @param chance_ Chance determines how often on average trades can be performed
+    /// @param spendADay_ determines how much ETH on average can Trader sell
+    /// @param low_ is a lower bound of sold ETH for a single trade
+    /// @param high_ is a higher bound of sold ETH for a single trade
+    function setSpendADay(uint256 chance_, uint256 spendADay_, uint256 low_, uint256 high_) public onlyOwner {
+        chance = chance_;
+        spendADay = spendADay_;
+        startingBlock = block.number;
+        lastHeight = block.number;
+        spent = 0;
+        saleValueLow = low_;
+        saleValueHigh = high_;
+    }
+
+    /// @notice Get random value for particular blockchain height.
+    /// @param height Height is block height to be used as a source of randomness. Will raise if called for blocks older than 256.
+    /// @return a pseudorandom uint256 value in range [0, type(uint256).max]
+    function getRandomNumber(uint256 height) public view returns (uint256) {
+        uint256 seed = uint256(blockhash(height));
+        if (seed == 0) revert Trader__RandomnessUnsafeSeed();
+        return randomize(seed);
+    }
+
+    function randomize(uint256 seed) private pure returns (uint256) {
+        return uint256(keccak256(abi.encode("Octant", seed)));
+    }
+
+    /// @dev This function returns random value distributed uniformly in range [low, high).
+    ///      Note, some values will not be chosen because of precision compromise.
+    ///      Also, if high > 2**200, this function may overflow.
+    /// @param low Low range of values returned
+    function getUniformInRange(uint256 low, uint256 high, uint256 seed) public pure returns (uint256) {
+        return low + ((high - low) * (randomize(seed) >> 200) / 2 ** (256 - 200));
+    }
+
+    receive() external payable {}
+}
---
+++ test/routers-transformers/Trader.s.sol
@@ -0,0 +1,156 @@
+/* SPDX-License-Identifier: UNLICENSED */
+pragma solidity ^0.8.23;
+
+import "forge-std/Test.sol";
+import "src/routers-transformers/Trader.sol";
+import {HelperConfig} from "script/helpers/HelperConfig.s.sol";
+
+contract TraderWrapper is Test {
+    Trader public trader;
+    uint256 public blockno = 1;
+    uint256 public constr = 0;
+    uint256 public budget = 10_000 ether;
+    HelperConfig helperConfig = new HelperConfig();
+
+    function setUp() external {
+        trader = new Trader();
+        trader.setSpendADay(0, 0, 0.6 ether, 1.4 ether);
+        vm.deal(address(trader), budget);
+    }
+
+    receive() external payable {}
+
+    // sequential call to this function emulate time progression
+    function wrapBuy() public returns (bool) {
+        vm.roll(blockno);
+        blockno = blockno + 1;
+        try trader.convert(block.number - 1) {
+            return true;
+        } catch (bytes memory) /*lowLevelData*/ {
+            return false;
+        }
+    }
+
+    function test_simpleBuy() external {
+        // effectively disable randomness check
+        uint256 chance = type(uint256).max;
+        // effectively disable upper bound check
+        uint256 spendADay = 1_000_000_000_000 ether;
+        trader.setSpendADay(chance, spendADay, 1 ether, 1 ether);
+        vm.roll(block.number + 100);
+        trader.convert(block.number - 2);
+        assertEq(trader.spent(), 1 ether);
+    }
+
+    function test_receivesEth() external {
+        (bool sent,) = payable(address(trader)).call{value: 100000}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    function test_wrapBuyBoundedWithRange() external {
+        uint256 chance = type(uint256).max / uint256(4000); // corresponds to 0.00025 chance
+        uint256 spendADay = 1 ether;
+        trader.setSpendADay(chance, spendADay, 0.6 ether, 1.4 ether);
+        uint256 blocks = 100_000;
+        for (uint256 i = 0; i < blocks; i++) {
+            wrapBuy();
+        }
+
+        // check if spending above target minus two buys
+        assertLt(((blocks * 1 ether) / trader.blocksADay()) - 2 ether, trader.spent());
+
+        // check if spending below target plus one buy
+        assertLt(trader.spent(), 1.4 ether + ((blocks * 1 ether) / trader.blocksADay()));
+    }
+
+    function test_wrapBuyBounded() external {
+        uint256 chance = type(uint256).max / uint256(4000); // corresponds to 0.00025 chance
+        uint256 spendADay = 1 ether;
+        trader.setSpendADay(chance, spendADay, 1 ether, 1 ether);
+        uint256 blocks = 100_000;
+        for (uint256 i = 0; i < blocks; i++) {
+            wrapBuy();
+        }
+
+        // check if spending above target minus two buys
+        assertLt(((blocks * 1 ether) / trader.blocksADay()) - 2 ether, trader.spent());
+
+        // check if spending below target plus one buy
+        assertLt(trader.spent(), 1 ether + ((blocks * 1 ether) / trader.blocksADay()));
+    }
+
+    function test_wrapBuyUnbounded() external {
+        uint256 chance = type(uint256).max / uint256(4000); // corresponds to 0.00025 chance
+        uint256 spendADay = 100 ether;
+        trader.setSpendADay(chance, spendADay, 1 ether, 1 ether);
+        uint256 blocks = 100_000;
+        for (uint256 i = 0; i < blocks; i++) {
+            wrapBuy();
+        }
+
+        // comparing to bounded test, average spending will be significantly higher
+        // proving that bounding with `spendADay` works
+        assertLt(((blocks * 1.4 ether) / trader.blocksADay()) - 2 ether, trader.spent());
+        assertLt(trader.spent(), 2 ether + ((blocks * 1.5 ether) / trader.blocksADay()));
+    }
+
+    function test_division() external pure {
+        assertLt(0, type(uint256).max / uint256(4000));
+        assertLt(type(uint256).max / uint256(4000), type(uint256).max / uint256(3999));
+        uint256 maxdiv4096 = 28269553036454149273332760011886696253239742350009903329945699220681916416;
+        assertGt(type(uint256).max / uint256(4095), maxdiv4096);
+        assertLt(type(uint256).max / uint256(4097), maxdiv4096);
+    }
+
+    function test_getUniformInRange_wei() public view {
+        runner_getUniformInRange(0, 256);
+    }
+
+    function test_getUniformInRange_ether() public view {
+        runner_getUniformInRange(100 ether, 200 ether);
+    }
+
+    function test_getUniformInRange_highEthers() public view {
+        runner_getUniformInRange(1_000_000 ether, 2_000_000 ether);
+    }
+
+    function runner_getUniformInRange(uint256 low, uint256 high) public view {
+        uint256 counter = 0;
+        uint256 val = 0;
+        uint256 min = type(uint256).max;
+        uint256 max = type(uint256).min;
+        uint256 mid = uint256((high + low) / 2);
+
+        for (uint256 i = 0; i < 100_000; i++) {
+            val = trader.getUniformInRange(low, high, i);
+            if (val < mid) counter = counter + 1;
+            if (val > max) max = val;
+            if (val < min) min = val;
+        }
+        assertLt(49_500, counter); // EV(counter) ~= 50_000
+        assertLt(counter, 51_500); // EV(counter) ~= 50_000
+        if (high - low < 1000) {
+            assertEq(min, low);
+            assertEq(max, high - 1);
+        }
+    }
+
+    function test_getUniformInRange_narrow() public view {
+        assertEq(1 ether, trader.getUniformInRange(1 ether, 1 ether, 4));
+    }
+
+    function test_blockHashValues() public {
+        vm.roll(20);
+        for (uint256 i = 1; i < 11; i++) {
+            assert(blockhash(block.number - i) != blockhash(block.number - i - 1));
+            assert(trader.getRandomNumber(block.number - i) != trader.getRandomNumber(block.number - i - 1));
+        }
+    }
+
+    function test_futureBlockHashValues() public {
+        vm.roll(20);
+        assert(blockhash(block.number - 1) != bytes32(0));
+        assert(blockhash(block.number) == bytes32(0));
+        assert(blockhash(block.number + 1) == bytes32(0));
+    }
+}
