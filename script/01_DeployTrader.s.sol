dc0b7e770deeda92f21373994057c84331506fb2
Pawel Peregud <paulperegud@protonmail.ch>
2024-10-11 14:34:40 +0200

    add Trader - it enables swap from ETH to GLM at random times

---
+++ script/01_DeployTrader.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+import {HelperConfig} from "./helpers/HelperConfig.s.sol";
+import {Trader} from "../src/routers-transformers/Trader.sol";
+
+contract DeployTraderHelper is Script {
+    function run() external {
+        (,,, uint256 deployerKey,,,) = new HelperConfig().activeNetworkConfig();
+
+        vm.startBroadcast(deployerKey);
+
+        Trader trader = new Trader();
+
+        vm.stopBroadcast();
+
+        console.log("Trader deployed to: ", address(trader));
+    }
+}
