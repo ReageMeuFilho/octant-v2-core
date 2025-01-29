48c78c15ead71f8f4dc78f1610ab4fbefbca2666
Pawel Peregud <paulperegud@protonmail.ch>
2025-01-29 18:53:10 +0100

    nix: install foundry via nix

--- flake.nix
+++ flake.nix
@@ -4,15 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/monthly";
   };
 
-  outputs = {nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = {nixpkgs, flake-utils, foundry, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ foundry.overlay ];
+      };
       darwinInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.xcbuild ];
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = [
+          pkgs.foundry-bin
           pkgs.nodejs_22.pkgs.yarn
           pkgs.nodejs_22
           pkgs.act # enables running GH pipeline locally
