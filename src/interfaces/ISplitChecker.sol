e9aa4a48dd4927b94371e0820489a028ea26ba92
Maxime <maximeviard@hotmail.fr>
2025-03-03 14:36:51 +0000

    refactor: add params comments to Split

--- src/interfaces/ISplitChecker.sol
+++ src/interfaces/ISplitChecker.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.8.18;
 
 interface ISplitChecker {
     struct Split {
-        address[] recipients;
-        uint256[] allocations;
-        uint256 totalAllocations;
+        address[] recipients; // [r1, r2, ..., opexVault, metapool]
+        uint256[] allocations; // should be in SPLIT_PRECISION terms
+        uint256 totalAllocations; // should be in SPLIT_PRECISION terms
     }
 
     function checkSplit(Split memory split, address opexVault, address metapool) external view;
