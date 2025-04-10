2d93e005ff16c6575d52522168f21fcc2f8af4a3
Ferit <ferit@cryptolab.net>
2025-04-10 12:23:00 +0300

    refactor(allowance): move structs, event and errors to the interface

--- src/dragons/modules/LinearAllowanceSingletonForGnosisSafe.sol
+++ src/dragons/modules/LinearAllowanceSingletonForGnosisSafe.sol
@@ -5,7 +5,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Enum } from "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { NATIVE_TOKEN } from "../../constants.sol";
-import { ILinearAllowanceSingletonForGnosisSafe } from "../../interfaces/ILinearAllowanceSingletonForGnosisSafe.sol";
+import { ILinearAllowanceSingleton } from "../../interfaces/ILinearAllowanceSingleton.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 interface ISafe {
@@ -17,49 +17,18 @@ interface ISafe {
     ) external returns (bool success);
 }
 
-event AllowanceSet(address indexed safe, address indexed delegate, address indexed token, uint256 dripRatePerDay);
-event AllowanceTransferred(
-    address indexed safe,
-    address indexed delegate,
-    address indexed token,
-    address to,
-    uint256 amount
-);
-
-error NoAllowanceToTransfer(address safe, address delegate, address token);
-error TransferFailed(address safe, address delegate, address token);
-
 /// @title LinearAllowanceSingletonForGnosisSafe
 /// @notice See ILinearAllowanceSingletonForGnosisSafe
-contract LinearAllowanceSingletonForGnosisSafe is ILinearAllowanceSingletonForGnosisSafe, ReentrancyGuard {
+contract LinearAllowanceSingletonForGnosisSafe is ILinearAllowanceSingleton, ReentrancyGuard {
     using SafeCast for uint256;
     using SafeCast for uint192;
     using SafeCast for uint160;
     using SafeCast for uint128;
     using SafeCast for uint32;
 
-    // Packed struct to use storage slots efficiently
-    struct LinearAllowance {
-        uint128 dripRatePerDay; // Max value is 3.40e+38 approximately.
-        uint160 totalUnspent; // Max value is 1.46e+48 approximately.
-        uint192 totalSpent; // Max value is 6.27e+57 approximately
-        uint32 lastBookedAtInSeconds; // Overflows in 2106.
-    }
-
     mapping(address => mapping(address => mapping(address => LinearAllowance))) public allowances; // safe -> delegate -> token -> allowance
 
-    function _updateAllowance(LinearAllowance memory a) internal view returns (LinearAllowance memory) {
-        if (a.lastBookedAtInSeconds != 0) {
-            uint256 timeElapsed = block.timestamp - a.lastBookedAtInSeconds;
-            uint256 daysElapsed = timeElapsed / 1 days;
-            a.totalUnspent += (daysElapsed * a.dripRatePerDay).toUint160();
-        }
-
-        a.lastBookedAtInSeconds = block.timestamp.toUint32();
-        return a;
-    }
-
-    /// @inheritdoc ILinearAllowanceSingletonForGnosisSafe
+    /// @inheritdoc ILinearAllowanceSingleton
     function setAllowance(address delegate, address token, uint128 dripRatePerDay) external {
         // Cache storage struct in memory to save gas
         LinearAllowance memory a = allowances[msg.sender][delegate][token];
@@ -74,7 +43,8 @@ contract LinearAllowanceSingletonForGnosisSafe is ILinearAllowanceSingletonForGn
         emit AllowanceSet(msg.sender, delegate, token, dripRatePerDay);
     }
 
-    /// @inheritdoc ILinearAllowanceSingletonForGnosisSafe
+    /// @inheritdoc ILinearAllowanceSingleton
+    /// @param safe The address of the safe which is the source of the allowance
     function executeAllowanceTransfer(
         address safe,
         address token,
@@ -131,7 +101,8 @@ contract LinearAllowanceSingletonForGnosisSafe is ILinearAllowanceSingletonForGn
         return transferAmount;
     }
 
-    /// @inheritdoc ILinearAllowanceSingletonForGnosisSafe
+    /// @inheritdoc ILinearAllowanceSingleton
+    /// @param safe The address of the safe which is the source of the allowance
     function getTokenAllowanceData(
         address safe,
         address delegate,
@@ -150,7 +121,8 @@ contract LinearAllowanceSingletonForGnosisSafe is ILinearAllowanceSingletonForGn
         );
     }
 
-    /// @inheritdoc ILinearAllowanceSingletonForGnosisSafe
+    /// @inheritdoc ILinearAllowanceSingleton
+    /// @param safe The address of the safe which is the source of the allowance
     function getTotalUnspent(address safe, address delegate, address token) public view returns (uint256) {
         // Cache the storage value in memory (single SLOAD)
         LinearAllowance memory allowance = allowances[safe][delegate][token];
@@ -164,5 +136,16 @@ contract LinearAllowanceSingletonForGnosisSafe is ILinearAllowanceSingletonForGn
         uint256 daysElapsed = timeElapsed / 1 days;
 
         return allowance.totalUnspent + allowance.dripRatePerDay * daysElapsed;
+    }
+
+    function _updateAllowance(LinearAllowance memory a) internal view returns (LinearAllowance memory) {
+        if (a.lastBookedAtInSeconds != 0) {
+            uint256 timeElapsed = block.timestamp - a.lastBookedAtInSeconds;
+            uint256 daysElapsed = timeElapsed / 1 days;
+            a.totalUnspent += (daysElapsed * a.dripRatePerDay).toUint160();
+        }
+
+        a.lastBookedAtInSeconds = block.timestamp.toUint32();
+        return a;
     }
 }
