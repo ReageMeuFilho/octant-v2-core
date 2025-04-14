7daf8e2cb6f6fb4f81118c6ea553e59c1064ffc1
Maxime <maximeviard@hotmail.fr>
2025-04-14 13:21:22 +0100

    lint: disable compiler-version & gas-custom-errors

--- test/mocks/MockSafeDragonRouter.sol
+++ test/mocks/MockSafeDragonRouter.sol
@@ -3,13 +3,12 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract MockSafeDragonRouter {
     address public splitChecker;
-    
+
     constructor(address _splitChecker) {
         splitChecker = _splitChecker;
     }
-    
+
     function setUp(address _splitChecker) public {
         splitChecker = _splitChecker;
     }
-
 }
