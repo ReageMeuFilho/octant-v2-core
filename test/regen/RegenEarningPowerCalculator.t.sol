e696f832f0e2ed991adea34201d6d85206d1499d
Ferit <ferit@cryptolab.net>
2025-05-29 13:23:02 +0100

    test(epc): refine test, introduce fuzzing, eliminate redundant tests, increase clarity

--- test/regen/RegenEarningPowerCalculator.t.sol
+++ test/regen/RegenEarningPowerCalculator.t.sol
@@ -24,9 +24,10 @@ contract RegenEarningPowerCalculatorTest is Test {
         staker2 = makeAddr("staker2");
         nonOwner = makeAddr("nonOwner");
 
+        vm.startPrank(owner);
         whitelist = new Whitelist();
-        vm.prank(owner);
         calculator = new RegenEarningPowerCalculator(owner, whitelist);
+        vm.stopPrank();
     }
 
     function test_Constructor_SetsOwner() public view {
@@ -55,273 +56,165 @@ contract RegenEarningPowerCalculatorTest is Test {
         assertTrue(calculator.supportsInterface(type(IERC165).interfaceId));
     }
 
-    function test_GetEarningPower_WhitelistDisabled() public {
+    function testFuzz_GetEarningPower_WhitelistDisabled(uint256 stakedAmount) public {
         vm.prank(owner);
         calculator.setWhitelist(IWhitelist(address(0))); // Disable whitelist
 
-        uint256 stakedAmount = 100e18;
         uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
-        assertEq(earningPower, stakedAmount, "EP should be stakedAmount when whitelist disabled");
+        if (stakedAmount > type(uint96).max) {
+            assertEq(earningPower, type(uint96).max, "EP should be capped at uint96 max");
+        } else {
+            assertEq(earningPower, stakedAmount, "EP should be stakedAmount when whitelist disabled");
+        }
     }
 
-    function test_GetEarningPower_UserWhitelisted() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-
-        uint256 stakedAmount = 100e18;
-        uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
-        assertEq(earningPower, stakedAmount, "EP should be stakedAmount for whitelisted user");
-    }
-
-    function test_GetEarningPower_UserNotWhitelisted() public view {
-        uint256 stakedAmount = 100e18;
-        uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
-        assertEq(earningPower, 0);
-    }
-
-    function test_GetEarningPower_ZeroStake_Whitelisted() public {
-        _whitelistAddress(staker1);
-        uint256 earningPower = calculator.getEarningPower(0, staker1, address(0));
-        assertEq(earningPower, 0);
-    }
-
-    function test_GetEarningPower_ZeroStake_WhitelistDisabled() public {
+    function testFuzz_GetEarningPower_UserWhitelisted(uint256 stakedAmount) public {
         vm.prank(owner);
-        calculator.setWhitelist(IWhitelist(address(0)));
-        uint256 earningPower = calculator.getEarningPower(0, staker1, address(0));
+        whitelist.addToWhitelist(staker1);
+
+        uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
+        if (stakedAmount > type(uint96).max) {
+            assertEq(earningPower, type(uint96).max, "EP should be capped at uint96 max");
+        } else {
+            assertEq(earningPower, stakedAmount, "EP should be stakedAmount for whitelisted user");
+        }
+    }
+
+    function testFuzz_GetEarningPower_UserNotWhitelisted(uint256 stakedAmount) public view {
+        uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
         assertEq(earningPower, 0);
     }
 
-    function test_GetEarningPower_CappedAtUint96Max() public {
+    function testFuzz_GetNewEarningPower_ChangesWhitelist(uint256 initialStakedAmount, uint256 oldEP) public {
+        vm.assume(initialStakedAmount <= type(uint96).max);
+        vm.assume(oldEP <= type(uint96).max);
+        vm.assume(oldEP > 0); // Ensure oldEP > 0 so changing whitelist causes a change
+
         vm.prank(owner);
-        calculator.setWhitelist(IWhitelist(address(0))); // Disable whitelist to focus on capping behavior
-
-        uint256 stakedAmountOverMax = uint256(type(uint96).max) + 1000;
-        uint256 earningPowerOverMax = calculator.getEarningPower(stakedAmountOverMax, staker1, address(0));
-        assertEq(earningPowerOverMax, type(uint96).max);
-
-        uint256 stakedAmountExactlyMax = type(uint96).max;
-        uint256 earningPowerExactlyMax = calculator.getEarningPower(stakedAmountExactlyMax, staker1, address(0));
-        assertEq(earningPowerExactlyMax, type(uint96).max);
-    }
-
-    function test_GetEarningPower_CappedAtUint96Max_UserWhitelisted() public {
-        _whitelistAddress(staker1);
-        uint256 stakedAmount = uint256(type(uint96).max) + 1000;
-        uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
-        assertEq(earningPower, type(uint96).max);
-    }
-
-    function test_GetEarningPower_CappedAtUint96Max_UserNotWhitelisted() public view {
-        // staker1 is NOT whitelisted by default
-        uint256 stakedAmount = uint256(type(uint96).max) + 1000;
-        uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
-        assertEq(earningPower, 0);
-    }
-
-    function test_GetNewEarningPower_BecomesWhitelisted() public {
-        // staker1 initially not whitelisted (calculator.whitelist is empty for staker1), oldEP=0
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users); // Now staker1 is whitelisted on calculator.whitelist
-
-        _checkNewEarningPower(100e18, staker1, 0, 100e18, true, "BecomesWhitelisted");
-    }
-
-    function test_GetNewEarningPower_LosesWhitelist() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users); // staker1 is on the 'whitelist' instance used by calculator
-
-        uint256 initialStakedAmount = 100e18;
-        uint256 oldEP = 100e18; // Assumed EP when staker1 was whitelisted
+        whitelist.addToWhitelist(staker1);
 
         // Change calculator's whitelist to one where staker1 is NOT present
-        Whitelist newEmptyWhitelist = new Whitelist();
+        Whitelist newEmptyWhitelist;
+        vm.prank(owner);
+        newEmptyWhitelist = new Whitelist();
         vm.prank(owner);
         calculator.setWhitelist(newEmptyWhitelist);
 
-        _checkNewEarningPower(initialStakedAmount, staker1, oldEP, 0, true, "LosesWhitelist");
-    }
-
-    function test_GetNewEarningPower_StakeDoubled_Whitelisted() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-        _checkNewEarningPower(200e18, staker1, 100e18, 200e18, true, "StakeDoubled_Whitelisted");
-    }
-
-    function test_GetNewEarningPower_StakeHalved_Whitelisted() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-        _checkNewEarningPower(50e18, staker1, 100e18, 50e18, true, "StakeHalved_Whitelisted");
-    }
-
-    function test_GetNewEarningPower_StakeSignificantlyDecreased_Whitelisted() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-        // newEP * 2 (80e18) <= oldEP (100e18) is true
-        _checkNewEarningPower(40e18, staker1, 100e18, 40e18, true, "StakeSignificantlyDecreased_Whitelisted");
-    }
-
-    function test_GetNewEarningPower_NoChange_NoBump_Whitelisted() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-        _checkNewEarningPower(100e18, staker1, 100e18, 100e18, false, "NoChange_NoBump_Whitelisted");
-    }
-
-    function test_GetNewEarningPower_StakeBecomesZero_Whitelisted() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-        // oldEP > 0, newEP == 0 -> qualifies
-        _checkNewEarningPower(0, staker1, 100e18, 0, true, "StakeBecomesZero_Whitelisted");
-    }
-
-    function test_GetNewEarningPower_StakeFromZero_Whitelisted() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-        // oldEP == 0, newEP > 0 -> qualifies
-        _checkNewEarningPower(100e18, staker1, 0, 100e18, true, "StakeFromZero_Whitelisted");
-    }
-
-    function test_GetNewEarningPower_WhitelistDisabled_StakeDoubled() public {
-        vm.prank(owner);
-        calculator.setWhitelist(IWhitelist(address(0)));
-        _checkNewEarningPower(200e18, staker1, 100e18, 200e18, true, "WhitelistDisabled_StakeDoubled");
-    }
-
-    function test_GetNewEarningPower_WhitelistDisabled_SmallChangeNoBump() public {
-        vm.prank(owner);
-        calculator.setWhitelist(IWhitelist(address(0)));
-        _checkNewEarningPower(110e18, staker1, 100e18, 110e18, true, "WhitelistDisabled_SmallChangeNoBump");
-    }
-
-    function test_GetNewEarningPower_CappedAtUint96Max_BecomesEligible() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        whitelist.addToWhitelist(users);
-        _checkNewEarningPower(
-            uint256(type(uint96).max) + 1000,
+        (uint256 newEP, bool qualifies) = calculator.getNewEarningPower(
+            initialStakedAmount,
             staker1,
-            10e18,
-            type(uint96).max,
-            true,
-            "CappedAtUint96Max_BecomesEligible"
+            address(0),
+            oldEP
         );
+        assertEq(newEP, 0, "New EP should be 0 after changing whitelist");
+        assertEq(qualifies, true, "Should qualify for bump after changing whitelist");
+    }
+
+    /// @notice Most complex test, covers almost all possible scenarios
+    function testFuzz_GetNewEarningPower_StakeChange(
+        uint256 newStake,
+        uint256 oldEarningPower,
+        bool isWhitelistEnabled,
+        bool isStakerWhitelisted
+    ) public {
+        vm.assume(oldEarningPower <= type(uint96).max);
+
+        // Setup whitelist state
+        if (!isWhitelistEnabled) {
+            vm.prank(owner);
+            calculator.setWhitelist(IWhitelist(address(0)));
+        } else if (isStakerWhitelisted) {
+            vm.prank(owner);
+            whitelist.addToWhitelist(staker1);
+        }
+
+        // Calculate expected new earning power
+        uint256 expectedNewEP;
+        if (!isWhitelistEnabled || isStakerWhitelisted) {
+            expectedNewEP = newStake > type(uint96).max ? type(uint96).max : newStake;
+        } else {
+            expectedNewEP = 0; // Not whitelisted
+        }
+        bool expectedQualifies = expectedNewEP != oldEarningPower;
+
+        (uint256 newEP, bool qualifies) = calculator.getNewEarningPower(newStake, staker1, address(0), oldEarningPower);
+
+        assertEq(newEP, expectedNewEP, "New earning power mismatch");
+        assertEq(qualifies, expectedQualifies, "Qualifies for bump mismatch");
+    }
+
+    function testFuzz_GetNewEarningPower_CappedAtUint96Max_BecomesEligible(uint256 stakedAmount, uint256 oldEP) public {
+        vm.assume(stakedAmount > type(uint96).max);
+        vm.assume(oldEP < type(uint96).max);
+
+        vm.prank(owner);
+        whitelist.addToWhitelist(staker1);
+
+        (uint256 newEP, bool qualifies) = calculator.getNewEarningPower(stakedAmount, staker1, address(0), oldEP);
+        assertEq(newEP, type(uint96).max, "New EP should be capped at uint96 max");
+        assertEq(qualifies, true, "Should qualify for bump when EP increases to cap");
     }
 
     function test_GetNewEarningPower_CappedAtUint96Max_RemainsEligible_NoBump() public {
-        _whitelistAddress(staker1);
+        vm.prank(owner);
+        whitelist.addToWhitelist(staker1);
+
         // Old and new EP are type(uint96).max, so no significant change.
-        _checkNewEarningPower(
-            uint256(type(uint96).max) + 1000,
+        uint256 stakedAmount = uint256(type(uint96).max) + 1000;
+        uint256 oldEarningPower = type(uint96).max;
+
+        (uint256 newEP, bool qualifies) = calculator.getNewEarningPower(
+            stakedAmount,
             staker1,
-            type(uint96).max,
-            type(uint96).max,
-            false,
-            "CappedAtUint96Max_RemainsEligible_NoBump"
+            address(0),
+            oldEarningPower
         );
-    }
-
-    function test_GetNewEarningPower_StakeDropsFromMax_Whitelisted() public {
-        _whitelistAddress(staker1);
-        _checkNewEarningPower(50e18, staker1, type(uint96).max, 50e18, true, "StakeDropsFromMax_Whitelisted");
-    }
-
-    function test_GetNewEarningPower_WhitelistDisabled_CappedAndQualifiesBump() public {
-        vm.prank(owner);
-        calculator.setWhitelist(IWhitelist(address(0)));
-        _checkNewEarningPower(
-            uint256(type(uint96).max) + 1,
-            staker1,
-            10e18,
-            type(uint96).max,
-            true,
-            "WhitelistDisabled_Capped_QualifiesBump"
-        );
-    }
-
-    function test_GetNewEarningPower_WhitelistDisabled_CappedNoBump() public {
-        vm.prank(owner);
-        calculator.setWhitelist(IWhitelist(address(0)));
-        _checkNewEarningPower(
-            uint256(type(uint96).max) + 1,
-            staker1,
-            type(uint96).max,
-            type(uint96).max,
-            false,
-            "WhitelistDisabled_Capped_NoBump"
-        );
-    }
-
-    function test_GetNewEarningPower_BothZero_NoBump() public {
-        address[] memory users = new address[](1);
-        users[0] = staker1;
-        // staker1 is NOT added to whitelist, so newEP will be 0 if whitelist is active
-        // oldEP is 0
-        _checkNewEarningPower(0, staker1, 0, 0, false, "BothZero_NoBump_NotWhitelisted");
-
-        whitelist.addToWhitelist(users); // Now staker1 is whitelisted
-        _checkNewEarningPower(0, staker1, 0, 0, false, "BothZero_NoBump_Whitelisted_StakeZero");
+        assertEq(newEP, type(uint96).max, "New EP should remain capped at uint96 max");
+        assertEq(qualifies, false, "Should not qualify for bump when EP remains at cap");
     }
 
     function test_SetWhitelist_AsOwner() public {
-        vm.startPrank(owner);
+        Whitelist newWhitelist = new Whitelist();
+
+        vm.prank(owner);
+        calculator.setWhitelist(newWhitelist);
+
+        assertEq(address(calculator.whitelist()), address(newWhitelist), "Whitelist should be updated");
+    }
+
+    function test_SetWhitelist_EmitsWhitelistSet() public {
         Whitelist newWhitelist = new Whitelist();
 
         vm.expectEmit();
         emit IWhitelistedEarningPowerCalculator.WhitelistSet(newWhitelist);
-        calculator.setWhitelist(newWhitelist);
-        vm.stopPrank();
-        assertEq(address(calculator.whitelist()), address(newWhitelist), "Whitelist should be updated");
-    }
 
-    function test_RevertIf_SetWhitelist_NotOwner() public {
-        Whitelist newWhitelist = new Whitelist();
-        vm.startPrank(nonOwner);
-        vm.expectRevert(abi.encodeWithSignature("OwnableUnauthorizedAccount(address)", nonOwner));
-        calculator.setWhitelist(newWhitelist);
-        vm.stopPrank();
-    }
-
-    function test_SetWhitelist_ToAddressZero_DisablesIt() public {
         vm.prank(owner);
+        calculator.setWhitelist(newWhitelist);
+    }
 
-        vm.expectEmit();
-        emit IWhitelistedEarningPowerCalculator.WhitelistSet(IWhitelist(address(0)));
+    function testFuzz_RevertIf_SetWhitelist_NotOwner(address notOwnerAddr) public {
+        vm.assume(notOwnerAddr != owner);
+
+        Whitelist newWhitelist = new Whitelist();
+        vm.startPrank(notOwnerAddr);
+        vm.expectRevert(abi.encodeWithSignature("OwnableUnauthorizedAccount(address)", notOwnerAddr));
+        calculator.setWhitelist(newWhitelist);
+        vm.stopPrank();
+    }
+
+    function testFuzz_SetWhitelist_ToAddressZero_DisablesIt(uint256 stakedAmount) public {
+        assertEq(calculator.whitelist().isWhitelisted(staker1), false, "Staker1 should not be whitelisted");
+
+        vm.prank(owner);
         calculator.setWhitelist(IWhitelist(address(0)));
-
         assertEq(address(calculator.whitelist()), address(0), "Whitelist should be address(0)");
 
         // Verify getEarningPower reflects this (user not on any whitelist, but whitelist is disabled)
-        uint256 stakedAmount = 100e18;
         uint256 earningPower = calculator.getEarningPower(stakedAmount, staker1, address(0));
-        assertEq(earningPower, stakedAmount, "EP should be stakedAmount when whitelist is address(0)");
-    }
-
-    // Helper for getNewEarningPower tests
-    function _checkNewEarningPower(
-        uint256 stakedAmount,
-        address stakerAddr,
-        uint256 oldEP,
-        uint256 expectedNewEP,
-        bool expectedQualifiesForBump,
-        string memory reason // Added reason for better error messages
-    ) internal view {
-        (uint256 newEP, bool qualifies) = calculator.getNewEarningPower(stakedAmount, stakerAddr, address(0), oldEP);
-        assertEq(newEP, expectedNewEP, string(abi.encodePacked("New EP mismatch: ", reason)));
-        assertEq(qualifies, expectedQualifiesForBump, string(abi.encodePacked("QualifiesForBump mismatch: ", reason)));
-    }
-
-    function _whitelistAddress(address addr) internal {
-        address[] memory users = new address[](1);
-        users[0] = addr;
-        whitelist.addToWhitelist(users);
+        if (stakedAmount > type(uint96).max) {
+            assertEq(earningPower, type(uint96).max, "EP should be capped at uint96 max");
+        } else {
+            assertEq(earningPower, stakedAmount, "EP should be stakedAmount when whitelist is address(0)");
+        }
     }
 }
