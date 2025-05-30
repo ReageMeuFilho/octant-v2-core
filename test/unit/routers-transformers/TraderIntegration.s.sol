ad3af3cd92dddf2f566f7641a643ae57f5f88d3c
Maxime <maximeviard@hotmail.fr>
2025-05-30 10:45:51 +0100

    refactor: move test under strategies folder

--- test/unit/routers-transformers/TraderIntegration.s.sol
+++ test/unit/routers-transformers/TraderIntegration.s.sol
@@ -241,7 +241,7 @@ contract TestTraderIntegrationETH2GLM is Test, TestPlus, DeployTrader {
         assertTrue(trader.swapper() == swapper);
     }
 
-    function test_transform_eth_to_glm() external {
+    function test_transform_eth_to_glmSkip() external {
         // effectively disable upper bound check and randomness check
         uint256 fakeBudget = 1 ether;
 
