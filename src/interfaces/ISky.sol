65102195e3829302f432c44852f363b1b18a56d3
Maxime <maximeviard@hotmail.fr>
2025-06-02 12:31:33 +0100

    refactor: create ISkyCompounder interface

--- src/interfaces/ISky.sol
+++ src/interfaces/ISky.sol
@@ -14,3 +14,19 @@ interface IStaking {
 interface IReferral {
     function deposit(uint256, address, uint16) external;
 }
+
+interface ISkyCompounder {
+    // Events for state changes
+    event ClaimRewardsUpdated(bool claimRewards);
+    event UniV3SettingsUpdated(bool useUniV3, uint24 rewardToBase, uint24 baseToAsset);
+    event MinAmountToSellUpdated(uint256 minAmountToSell);
+    event BaseTokenUpdated(address base, bool useUniV3, uint24 rewardToBase, uint24 baseToAsset);
+    event ReferralUpdated(uint16 referral);
+
+    // Management functions
+    function setClaimRewards(bool _claimRewards) external;
+    function setUseUniV3andFees(bool _useUniV3, uint24 _rewardToBase, uint24 _baseToAsset) external;
+    function setMinAmountToSell(uint256 _minAmountToSell) external;
+    function setBase(address _base, bool _useUniV3, uint24 _rewardToBase, uint24 _baseToAsset) external;
+    function setReferral(uint16 _referral) external;
+}
