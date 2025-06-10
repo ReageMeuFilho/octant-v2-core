2345c7f291ba6868142b30ef7ff0fed131d0c6cf
Maxime <maximeviard@hotmail.fr>
2025-06-10 17:54:02 +0100

    refactor: update LidoStrategy with BaseYieldSkimmingHealthCheck

--- src/strategies/yieldSkimming/LidoStrategy.sol
+++ src/strategies/yieldSkimming/LidoStrategy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.25;
 
-import { BaseHealthCheck } from "src/strategies/periphery/BaseHealthCheck.sol";
+import { BaseYieldSkimmingHealthCheck } from "src/strategies/periphery/BaseYieldSkimmingHealthCheck.sol";
 
 import { ITokenizedStrategy } from "src/core/interfaces/ITokenizedStrategy.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -17,17 +17,11 @@ interface IWstETH {
  * @notice A strategy that manages deposits in a Lido yield source and captures yield
  * @dev This strategy tracks the value of deposits and captures yield as the price per share increases
  */
-contract LidoStrategy is BaseHealthCheck {
+contract LidoStrategy is BaseYieldSkimmingHealthCheck {
     using SafeERC20 for IERC20;
 
     /// @dev The exchange rate at the last harvest, scaled by 1e18
     uint256 internal _lastReportedExchangeRate;
-
-    // Default profit limit to 100%. // todo create functions to update them (profit and loss)
-    uint16 private _profitLimitRatio = uint16(MAX_BPS);
-
-    // Defaults loss limit to 0.
-    uint16 private _lossLimitRatio;
 
     /// @notice yearn governance
     address public constant GOV = 0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52;
@@ -46,7 +40,7 @@ contract LidoStrategy is BaseHealthCheck {
         address _donationAddress,
         address _tokenizedStrategyAddress
     )
-        BaseHealthCheck(
+        BaseYieldSkimmingHealthCheck(
             _asset, // shares address
             _name,
             _management,
@@ -58,16 +52,6 @@ contract LidoStrategy is BaseHealthCheck {
     {
         // Initialize the exchange rate on setup
         _lastReportedExchangeRate = _getCurrentExchangeRate();
-    }
-
-    /**
-     * @notice Update the profit limit ratio
-     * @param _newProfitLimitRatio The new profit limit ratio
-     */
-    function updateProfitLimitRatio(uint256 _newProfitLimitRatio) external onlyManagement {
-        require(_profitLimitRatio > 0, "!zero profit");
-        require(_profitLimitRatio <= type(uint16).max, "!too high");
-        _profitLimitRatio = uint16(_newProfitLimitRatio);
     }
 
     /// @notice Sweep of non-asset ERC20 tokens to governance (onlyGovernance)
@@ -102,14 +86,6 @@ contract LidoStrategy is BaseHealthCheck {
     }
 
     /**
-     * @notice Get the current profit limit ratio
-     * @return The profit limit ratio
-     */
-    function getProfitLimitRatio() public view returns (uint16) {
-        return _profitLimitRatio;
-    }
-
-    /**
      * @notice Deposits available funds into the yield vault
      * @param _amount Amount to deploy
      */
@@ -125,24 +101,6 @@ contract LidoStrategy is BaseHealthCheck {
         // nothing to do here as assets are always held in the strategy
     }
 
-    function _executeHealthCheck(uint256 _profit) internal override {
-        if (!doHealthCheck) {
-            doHealthCheck = true;
-            return;
-        }
-
-        // Get the current total assets from the implementation.
-        uint256 currentTotalAssets = TokenizedStrategy.totalAssets();
-
-        if (_profit > 0) {
-            uint256 previousTotalAssets = currentTotalAssets - _profit;
-            require(
-                _profit <= (previousTotalAssets * _profitLimitRatio) / MAX_BPS,
-                "healthCheck: profit limit exceeded"
-            );
-        }
-    }
-
     /**
      * @notice Withdraws funds from the yield vault
      * @param _amount Amount to free
@@ -153,26 +111,22 @@ contract LidoStrategy is BaseHealthCheck {
 
     /**
      * @notice Captures yield by calculating the increase in value based on exchange rate changes
-     * @return _totalAssets The current total assets of the strategy
+     * @return deltaInUnderlyingAssetValue The current delta of the strategy
      */
-    function _harvestAndReport() internal override returns (uint256) {
+    function _harvestAndReport() internal override returns (int256 deltaInUnderlyingAssetValue) {
         uint256 currentExchangeRate = _getCurrentExchangeRate();
 
-        // Get the current balance of assets in the strategy
-        uint256 assetBalance = IERC20(asset).balanceOf(address(this));
+        // Get the current balance of assets in the strategy (not using totalSupply so that it goes to profit)
+        uint256 assetBalance = ITokenizedStrategy(address(this)).totalAssets();
 
         // Calculate the profit based on exchange rate difference
-        uint256 deltaExchangeRate = currentExchangeRate > _lastReportedExchangeRate
-            ? currentExchangeRate - _lastReportedExchangeRate
-            : 0; // Only capture positive yield
+        int256 deltaExchangeRate = int256(currentExchangeRate) - int256(_lastReportedExchangeRate);
 
-        uint256 profitInValue = (assetBalance * deltaExchangeRate) / ERC20(asset).decimals();
+        int256 deltaInValue = int256(assetBalance) * deltaExchangeRate;
 
-        uint256 profitInYieldVaultShares = (profitInValue * ERC20(asset).decimals()) / currentExchangeRate;
+        deltaInUnderlyingAssetValue = deltaInValue / int256(currentExchangeRate);
 
         _lastReportedExchangeRate = currentExchangeRate;
-
-        return profitInYieldVaultShares;
     }
 
     /**
