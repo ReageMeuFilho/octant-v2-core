f948f687aa99df387062e157f11cc0d7c29358e1
Maxime <maximeviard@hotmail.fr>
2025-06-17 16:45:00 +0100

    refactor: update licence in LinearAllowanceSingletonForGnosisSafe

--- src/zodiac-core/LinearAllowanceExecutor.sol
+++ src/zodiac-core/LinearAllowanceExecutor.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import { LinearAllowanceSingletonForGnosisSafe } from "src/zodiac-core/modules/LinearAllowanceSingletonForGnosisSafe.sol";
