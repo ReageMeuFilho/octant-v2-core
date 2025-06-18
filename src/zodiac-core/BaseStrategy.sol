42f887ab042157062f27942b422e59a270ae4be8
Maxime <maximeviard@hotmail.fr>
2025-06-18 18:33:24 +0100

    refactor: remove _harvestAndReport from BaseStrategy

--- src/zodiac-core/BaseStrategy.sol
+++ src/zodiac-core/BaseStrategy.sol
@@ -136,22 +136,6 @@ abstract contract BaseStrategy {
     }
 
     /**
-     * @notice Returns the accurate amount of all funds currently
-     * held by the Strategy.
-     * @dev Callback for the TokenizedStrategy to call during a report to
-     * get an accurate accounting of assets the strategy controls.
-     *
-     * This can only be called after a report() delegateCall to the
-     * TokenizedStrategy so msg.sender == address(this).
-     *
-     * @return . A trusted and accurate account for the total amount
-     * of 'asset' the strategy currently holds including idle funds.
-     */
-    function harvestAndReport() external virtual onlySelf returns (uint256) {
-        return _harvestAndReport();
-    }
-
-    /**
      * @notice Will call the internal '_tend' when a keeper tends the strategy.
      * @dev Callback for the TokenizedStrategy to initiate a _tend call in the strategy.
      *
@@ -285,30 +269,6 @@ abstract contract BaseStrategy {
      * @param _amount, The amount of 'asset' to be freed.
      */
     function _freeFunds(uint256 _amount) internal virtual;
-
-    /**
-     * @dev Internal function to harvest all rewards, redeploy any idle
-     * funds and return an accurate accounting of all funds currently
-     * held by the Strategy.
-     *
-     * This should do any needed harvesting, rewards selling, accrual,
-     * redepositing etc. to get the most accurate view of current assets.
-     *
-     * NOTE: All applicable assets including loose assets should be
-     * accounted for in this function.
-     *
-     * Care should be taken when relying on oracles or swap values rather
-     * than actual amounts as all Strategy profit/loss accounting will
-     * be done based on this returned value.
-     *
-     * This can still be called post a shutdown, a strategist can check
-     * `TokenizedStrategy.isShutdown()` to decide if funds should be
-     * redeployed or simply realize any profits/losses.
-     *
-     * @return _totalAssets A trusted and accurate account for the total
-     * amount of 'asset' the strategy currently holds including idle funds.
-     */
-    function _harvestAndReport() internal virtual returns (uint256 _totalAssets);
 
     /*//////////////////////////////////////////////////////////////
                     OPTIONAL TO OVERRIDE BY STRATEGIST
