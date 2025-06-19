72ee74cd2f3122fda00d5366ff3534ed0a223d19
Maxime <maximeviard@hotmail.fr>
2025-06-19 20:29:38 +0100

    feat: update LidoStrategy with BaseYieldSkimming

--- src/strategies/yieldSkimming/LidoStrategy.sol
+++ src/strategies/yieldSkimming/LidoStrategy.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.25;
 
-import { BaseYieldSkimmingHealthCheck } from "src/strategies/periphery/BaseYieldSkimmingHealthCheck.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { BaseYieldSkimmingStrategy } from "src/strategies/yieldSkimming/BaseYieldSkimmingStrategy.sol";
 
 interface IWstETH {
     function stEthPerToken() external view returns (uint256);
@@ -14,17 +12,7 @@ interface IWstETH {
  * @notice A strategy that manages deposits in a Lido yield source and captures yield
  * @dev This strategy tracks the value of deposits and captures yield as the price per share increases
  */
-contract LidoStrategy is BaseYieldSkimmingHealthCheck {
-    using SafeERC20 for IERC20;
-
-    /// @notice yearn governance
-    address public constant GOV = 0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52;
-
-    modifier onlyGovernance() {
-        require(msg.sender == GOV, "!gov");
-        _;
-    }
-
+contract LidoStrategy is BaseYieldSkimmingStrategy {
     constructor(
         address _asset,
         string memory _name,
@@ -34,7 +22,7 @@ contract LidoStrategy is BaseYieldSkimmingHealthCheck {
         address _donationAddress,
         address _tokenizedStrategyAddress
     )
-        BaseYieldSkimmingHealthCheck(
+        BaseYieldSkimmingStrategy(
             _asset, // shares address
             _name,
             _management,
@@ -45,84 +33,12 @@ contract LidoStrategy is BaseYieldSkimmingHealthCheck {
         )
     {}
 
-    /// @notice Sweep of non-asset ERC20 tokens to governance (onlyGovernance)
-    /// @param _token The ERC20 token to sweep
-    function sweep(address _token) external onlyGovernance {
-        require(_token != address(asset), "!asset");
-        IERC20(_token).safeTransfer(GOV, IERC20(_token).balanceOf(address(this)));
-    }
-
-    /**
-     * @notice Get the current balance of the asset
-     * @return The asset balance in this contract
-     */
-    function balanceOfAsset() public view returns (uint256) {
-        return IERC20(asset).balanceOf(address(this));
-    }
-
-    /**
-     * @notice Get the current balance of shares in the yield vault
-     * @return The yield vault share balance
-     */
-    function balanceOfShares() public view returns (uint256) {
-        return IERC20(asset).balanceOf(address(this));
-    }
-
-    /**
-     * @notice Deposits available funds into the yield vault
-     * @param _amount Amount to deploy
-     */
-    function _deployFunds(uint256 _amount) internal override {
-        // no action needed
-    }
-
-    /**
-     * @notice Emergency withdrawal function to transfer tokens to emergency admin
-     * @param _amount Amount to withdraw in emergency
-     */
-    function _emergencyWithdraw(uint256 _amount) internal override {
-        // nothing to do here as assets are always held in the strategy
-    }
-
-    /**
-     * @notice Withdraws funds from the yield vault
-     * @param _amount Amount to free
-     */
-    function _freeFunds(uint256 _amount) internal override {
-        // no action needed
-    }
-
-    /**
-     * @notice Captures yield by calculating the increase in value based on exchange rate changes
-     * @return _totalAssets The current total assets of the strategy
-     */
-    function _harvestAndReport() internal override returns (uint256 _totalAssets) {}
-
-    /**
-     * @notice No tending needed
-     */
-    function _tend(uint256 /*_idle*/) internal override {
-        // No action needed
-    }
-
-    function getCurrentExchangeRate() public view returns (uint256) {
-        return _getCurrentExchangeRate();
-    }
-
     /**
      * @notice Gets the current exchange rate from the yield vault
      * @return The current price per share
      */
-    function _getCurrentExchangeRate() internal view virtual returns (uint256) {
-        // Call the pricePerShare function on the yield vault
+    function _getCurrentExchangeRate() internal view override returns (uint256) {
+        // Call the stEthPerToken function on the wstETH contract
         return IWstETH(address(asset)).stEthPerToken();
-    }
-
-    /**
-     * @notice Always returns false as no tending is needed
-     * @return Always false as tending is not required
-     */
-    function _tendTrigger() internal pure override returns (bool) {
-        return false;
     }
 }
