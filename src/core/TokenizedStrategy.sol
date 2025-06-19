05be84ca9b04d21c4d66558e8b32296a67c67c13
Maxime <maximeviard@hotmail.fr>
2025-06-19 19:57:15 +0100

    refactor: add virtual keyword to functions in TokenizedStrategy

--- src/core/TokenizedStrategy.sol
+++ src/core/TokenizedStrategy.sol
@@ -403,7 +403,7 @@ abstract contract TokenizedStrategy {
         address _keeper,
         address _emergencyAdmin,
         address _dragonRouter
-    ) external {
+    ) public virtual {
         // Cache storage pointer.
         StrategyData storage S = _strategyStorage();
 
@@ -761,7 +761,7 @@ abstract contract TokenizedStrategy {
         StrategyData storage S,
         uint256 assets,
         Math.Rounding _rounding
-    ) internal view returns (uint256) {
+    ) internal view virtual returns (uint256) {
         // Saves an extra SLOAD if values are non-zero.
         uint256 totalSupply_ = _totalSupply(S);
         // If supply is 0, PPS = 1.
@@ -779,7 +779,7 @@ abstract contract TokenizedStrategy {
         StrategyData storage S,
         uint256 shares,
         Math.Rounding _rounding
-    ) internal view returns (uint256) {
+    ) internal view virtual returns (uint256) {
         // Saves an extra SLOAD if totalSupply() is non-zero.
         uint256 supply = _totalSupply(S);
 
