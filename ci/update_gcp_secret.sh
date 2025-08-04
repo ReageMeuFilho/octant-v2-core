8d1c303b381bdd60dcb33fea7faaae3f26c672ee
Marek Słomnicki <marek@golem.foundation>
2025-08-04 16:50:29 +0200

    chore: fix pipeline

--- ci/update_gcp_secret.sh
+++ ci/update_gcp_secret.sh
@@ -9,10 +9,10 @@ echo "$GOOGLE_APPLICATION_CREDENTIALS_B64" | base64 -d >.gcloud-credentials.json
 chmod 400 .gcloud-credentials.json
 gcloud auth activate-service-account --key-file=.gcloud-credentials.json
 
-jq -Rn '[inputs | select(length > 0) | split("=")] | map({ (.[0]): .[1] }) | add' < ci/contract_addresses.txt > ci/contract_addresses.json
+jq -Rn '[inputs | select(length > 0) | split("=")] | map({ (.[0]): .[1] }) | add' < contract_addresses.txt > contract_addresses.json
 
 # Update the GCP secret with the new version
-gcloud secrets versions add "octant-v2-${ENVIRONMENT}-contracts" --data-file=ci/contract_addresses.json --project="$GCP_PROJECT"
+gcloud secrets versions add "octant-v2-${ENVIRONMENT}-contracts" --data-file=contract_addresses.json --project="$GCP_PROJECT"
 
 # Clean up
-rm .gcloud-credentials.json ci/contract_addresses.json
+rm .gcloud-credentials.json contract_addresses.json
