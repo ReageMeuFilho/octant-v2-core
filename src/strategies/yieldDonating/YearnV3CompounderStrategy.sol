00b27d8a675e103afeabd61b04a5892f8810003b
Maxime <maximeviard@hotmail.fr>
2025-10-08 14:09:21 +0100

    feat: create YearnV3CompounderStrategy

---
+++ src/strategies/yieldDonating/YearnV3CompounderStrategy.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { BaseHealthCheck } from "src/strategies/periphery/BaseHealthCheck.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { ITokenizedStrategy } from "src/core/interfaces/ITokenizedStrategy.sol";
+
+/// @title YearnV3CompounderStrategy
+/// @author [Golem Foundation](https://golem.foundation)
+/// @custom:security-contact security@golem.foundation
+/// @notice Strategy that donates rewards from a Yearn v3 vault.
+contract YearnV3CompounderStrategy is BaseHealthCheck {
+    using SafeERC20 for IERC20;
+
+    // Yearn v3 vault
+    address public immutable yearnVault;
+
+    /**
+     * @param _yearnVault Address of the Yearn v3 vault this strategy compounds into
+     * @param _asset Address of the underlying asset of the Yearn v3 vault
+     * @param _name Strategy name
+     * @param _management Address with management role
+     * @param _keeper Address with keeper role
+     * @param _emergencyAdmin Address with emergency admin role
+     * @param _donationAddress Address that receives donated/minted yield
+     * @param _enableBurning Whether loss-protection burning from donation address is enabled
+     * @param _tokenizedStrategyAddress Address of TokenizedStrategy implementation
+     */
+    constructor(
+        address _yearnVault,
+        address _asset,
+        string memory _name,
+        address _management,
+        address _keeper,
+        address _emergencyAdmin,
+        address _donationAddress,
+        bool _enableBurning,
+        address _tokenizedStrategyAddress
+    )
+        BaseHealthCheck(
+            _asset,
+            _name,
+            _management,
+            _keeper,
+            _emergencyAdmin,
+            _donationAddress,
+            _enableBurning,
+            _tokenizedStrategyAddress
+        )
+    {
+        // make sure asset is Yearn vault's asset
+        require(ITokenizedStrategy(_yearnVault).asset() == _asset, "Asset mismatch with compounder vault");
+        IERC20(_asset).forceApprove(_yearnVault, type(uint256).max);
+        yearnVault = _yearnVault;
+    }
+
+    function availableDepositLimit(address /*_owner*/) public view override returns (uint256) {
+        // todo consider removing idle balance from the limit (tbd)
+        uint256 vaultLimit = ITokenizedStrategy(yearnVault).maxDeposit(address(this));
+        uint256 idleBalance = IERC20(asset).balanceOf(address(this));
+        return vaultLimit > idleBalance ? vaultLimit - idleBalance : 0;
+    }
+
+    function availableWithdrawLimit(address /*_owner*/) public view override returns (uint256) {
+        return IERC20(asset).balanceOf(address(this)) + ITokenizedStrategy(yearnVault).maxWithdraw(address(this));
+    }
+
+    function _deployFunds(uint256 _amount) internal override {
+        ITokenizedStrategy(yearnVault).deposit(_amount, address(this));
+    }
+
+    function _freeFunds(uint256 _amount) internal override {
+        ITokenizedStrategy(yearnVault).withdraw(_amount, address(this), address(this), 10_000); // 10_000 = 100% max loss
+    }
+
+    function _emergencyWithdraw(uint256 _amount) internal override {
+        _freeFunds(_amount);
+    }
+
+    function _harvestAndReport() internal view override returns (uint256 _totalAssets) {
+        // get strategy's balance in the vault
+        uint256 shares = ITokenizedStrategy(yearnVault).balanceOf(address(this));
+        uint256 vaultAssets = ITokenizedStrategy(yearnVault).convertToAssets(shares);
+
+        uint256 idleAssets = IERC20(asset).balanceOf(address(this));
+
+        _totalAssets = vaultAssets + idleAssets;
+
+        return _totalAssets;
+    }
+}
